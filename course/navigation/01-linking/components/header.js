caedbd5b3e176bcdd5ffd42d2b5862cd31d9ebe9
Adrian Thomas <adrian.l.thomas@outlook.com>
2020-11-08 16:12:59 -0600

    adds dynamic linking

--- course/navigation/01-linking/components/header.js
+++ course/navigation/01-linking/components/header.js
@@ -1,23 +1,34 @@
 import Link from 'next/link';
 
 export default () => (
-    <header>
-        <ul>
-            <li>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/about">
-                    <a>About</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/contact">
-                    <a>Contact</a>
-                </Link>
-            </li>
-        </ul>
-    </header>
+  <header>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+      </li>
+      {/* use "as" to tell nextJS which specific post is associated with the dynamic link */}
+      <li>
+        <Link as="/post/first-post" href="/post[id]">
+          <a>First Post</a>
+        </Link>
+      </li>
+      <li>
+        <Link as="/post/second-post" href="/post[id]">
+          <a>Second Post</a>
+        </Link>
+      </li>
+    </ul>
+  </header>
 );
