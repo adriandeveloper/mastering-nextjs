eaf762d802fea9da5b7655ebc28c66d51fe1aaf0
Adrian Thomas <adrian.l.thomas@outlook.com>
2020-11-08 08:10:10 -0600

    using fragments spread items and filter

--- course/react/05-full-example/pages/index.js
+++ course/react/05-full-example/pages/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 
-const List = ({ items }) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item}>{item}</li>
-    ))}
-  </ul>
-);
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filteredItems: this.props.items
+    };
+
+    this.filterItems = this.filterItems.bind(this);
+  }
+
+  filterItems(e) {
+    // what user typed in
+    const searchValue = e.target.value;
+    // full list of items. using spread operator to copy the arr without modifing properties directly
+    const currentItems = [...this.props.items];
+    const matchingItems = currentItems.filter((item) =>
+      item.startsWith(searchValue)
+    );
+
+    this.setState({
+      filteredItems: matchingItems
+    });
+  }
+  render() {
+    return (
+      // fragment? WTF is that?
+      <>
+        <input onChange={this.filterItems} />
+        <ul>
+          {this.state.filteredItems.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
 const ListContainer = () => (
-  <List items={['Learn React', 'Learn Next.js', '???', 'Profit!']} />
+  <List items={['Learn React', 'Learn Next.js', '???', 'Profit']} />
 );
 
 export default ListContainer;
